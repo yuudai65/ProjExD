3f43a6d957e227b7c4e45722b3a0747fb62ed148
C0B21004 <c0b210041b@edu.teu.ac.jp>
2022-04-26 16:33:30 +0900

    掛け算とかを追加

--- rensyu02/calc.py
+++ rensyu02/calc.py
@@ -19,16 +19,16 @@ if __name__ == "__main__":
     entry = tk.Entry(root, justify = "right", width = 10, font = ("Times New Roman", 40))
     entry.grid(row = 0, column = 0, columnspan=4)
     r, c = 1, 1
-    for i , num in enumerate([9,8,7,6,5,4,3,2,1,0,"+"], 1):
-        btn = tk.Button(root, text=num, font = ("Times New Roman", 30))
+    for i , num in enumerate(["+", "-","*","/",9,8,7,6,5,4,3,2,1,0,], 1):
+        btn = tk.Button(root, text=num, font = ("Times New Roman", 20))
         btn.bind("<1>", button_click)
         btn.grid(row = r, column = c, padx = 10, pady = 10)
         if i%3 ==0:
             r+= 1
             c = 0
         c += 1
-    btn = tk.Button(root,text="=", font = ("Times New Roman", 30))
+    btn = tk.Button(root,text="=", font = ("Times New Roman", 20))
     btn.bind("<1>", click_equal)
-    btn.grid(row = r,column = c,padx = 10, pady = 10)
+    btn.grid(row = r,column = c,padx = 5, pady = 10)
 
     root.mainloop()
