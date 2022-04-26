d14ec84f98a3f9945d272d129a01f7f3a6a317e5
C0B21004 <c0b210041b@edu.teu.ac.jp>
2022-04-26 14:36:02 +0900

    第一問

--- rensyu02/calc.py
+++ rensyu02/calc.py
@@ -3,4 +3,7 @@ import tkinter as tk
 if __name__ == "__main__":
     root = tk.Tk()
     root.geometry("300x450")
+    for i in range(0, 10):
+        suji = tk.Button(root, text= i, font = ("Times New Roman", 30))
+        suji.pack()
     root.mainloop()
