5fb1d2892b553e30722df8a288532cc0b41a33cb
C0B21004 <c0b210041b@edu.teu.ac.jp>
2022-04-26 15:05:49 +0900

    難しいよね

--- rensyu02/calc.py
+++ rensyu02/calc.py
@@ -1,9 +1,21 @@
 import tkinter as tk
+import tkinter.messagebox as tkm
+
+def button_click(event):
+    btn = event.widget
+    num = btn["text"]
+    tkm.showinfo(num, f"{num}のボタンがクリックされました")
 
 if __name__ == "__main__":
     root = tk.Tk()
     root.geometry("300x450")
-    for i in range(0, 10):
-        suji = tk.Button(root, text= i, font = ("Times New Roman", 30))
-        suji.pack()
+    r, c = 0, 1
+    for i , num in enumerate(range(9, -1, -1), 1):
+        btn = tk.Button(root, text=num, font = ("Times New Roman", 30))
+        btn.bind("<1>", button_click)
+        btn.grid(row = r, column = c, padx = 10, pady = 10)
+        if i%3 ==0:
+            r+= 1
+            c = 0
+        c += 1
     root.mainloop()
