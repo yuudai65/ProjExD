2c91d14e02f16fa94af639788c1aefef9ed36b7f
C0B21004 <c0b210041b@edu.teu.ac.jp>
2022-04-26 15:30:53 +0900

    足し算の記号の追加

--- rensyu02/calc.py
+++ rensyu02/calc.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     entry = tk.Entry(root, justify = "right", width = 10, font = ("Times New Roman", 40))
     entry.grid(row = 0, column = 0, columnspan=4)
     r, c = 1, 1
-    for i , num in enumerate(range(9, -1, -1), 1):
+    for i , num in enumerate([9,8,7,6,5,4,3,2,1,0,"+"], 1):
         btn = tk.Button(root, text=num, font = ("Times New Roman", 30))
         btn.bind("<1>", button_click)
         btn.grid(row = r, column = c, padx = 10, pady = 10)
