c90585fd19fcb0f09799b12801542e0d480b1411
C0B21004 <c0b210041b@edu.teu.ac.jp>
2022-05-10 14:50:28 +0900

    こうかとんが暗闇に

--- rensyu03/maze.py
+++ rensyu03/maze.py
@@ -1,9 +1,11 @@
 import tkinter as tk
 
 if __name__ == "__main__":
-    root = tk.Tk
+    root = tk.Tk()
     root.title("迷えるコウカトン")
-
     canvas = tk.Canvas(root, width = 1500, height = 900, bg = "black")
     canvas.pack()
+
+    tori = tk.PhotoImage(file = "fig/8.png")
+    cx, cy = 
     root.mainloop()
