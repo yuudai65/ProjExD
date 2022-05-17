bffb9b0dce163d95cd15565c2a0c8fab6bb97e61
C0B21004 <c0b210041b@edu.teu.ac.jp>
2022-05-17 15:51:46 +0900

    画面外に出ないようにした

--- rensyu04/dodge_bomb.py
+++ rensyu04/dodge_bomb.py
@@ -1,6 +1,14 @@
 import pygame as pg
 import sys
+import random
 
+key_delta = {
+             pg.K_UP:[0, -1],
+             pg.K_DOWN:[0, +1],
+             pg.K_RIGHT:[+1, 0],
+             pg.K_LEFT:[-1, 0],}
+
+             
 
 def main():
     clock = pg.time.Clock()
@@ -18,14 +26,54 @@ def main():
     tori_rect.center = 900, 400               #こうかとん画像用surfaceを画像用surfaceに貼り付ける
     screen.blit(tori_img, tori_rect)
 
+    #練習5
+    bomb = pg.Surface((20, 20))  #爆弾用のSurface
+    bomb.set_colorkey((0, 0, 0))
+    pg.draw.circle(bomb, (255, 0, 0), (10, 10), 10) #爆弾用Surfaceに円を描く
+    bomb_rect = bomb.get_rect()  #爆弾用rect
+    bomb_rect.centerx = random.randint(0, sc_rect.width)
+    bomb_rect.centery = random.randint(0, sc_rect.height)
+    screen.blit(bomb, bomb_rect) #爆弾用のsurfaceを画面用surfaceに張り付ける
+    vx, vy = +1, +1
+
     while True:
         #練習2
         screen.blit(bg_img, bg_rect)
         for event in pg.event.get():
            if event.type == pg.QUIT: return #×ボタンでmain関数から戻る
+
+        #練習4
+        key_status = pg.key.get_pressed()#key_statusは256もの状態がある
+        for key, delta in key_delta.items():
+            if key_status[key] == True:
+                tori_rect.centerx += delta[0]
+                tori_rect.centery += delta[1]
+                if check_bound(sc_rect, tori_rect) != (1,1):
+                    tori_rect.centerx -= delta[0]
+                    tori_rect.centery -= delta[1]
+        screen.blit(tori_img, tori_rect)
+    
+
+        #練習6
+        
+        bomb_rect.move_ip(vx, vy)
+        screen.blit(bomb, bomb_rect)
+        x,y = check_bound(sc_rect, bomb_rect)
+        vx *= x #横方向に画面外なら横方向速度の符号反転
+        vy *= y #縦方向に画面外なら縦方向速度の符号反転
+
+
         pg.display.update()  #画面を更新
         clock.tick(1000)
 
+def check_bound(sc_r, obj_r): #画面用rect, こうかとんか爆弾用rect
+    #画面内:+1 /　画面外:-1
+    x, y = +1, +1
+    if obj_r.left < sc_r.left or sc_r.right < obj_r.right: 
+        x = -1 #画面外
+    if obj_r.top < sc_r.top or sc_r.bottom < obj_r.bottom: 
+        y = -1
+    return x, y
 
 
 if __name__ == "__main__":
@@ -33,3 +81,7 @@ if __name__ == "__main__":
     main()
     pg.quit()
     sys.exit()
+
+#改造案
+#爆弾増やす
+#こうかとんの画像をランダムに
