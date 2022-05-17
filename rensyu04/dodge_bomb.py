62bdee9030d5330332e8af299654116104e8294b
C0B21004 <c0b210041b@edu.teu.ac.jp>
2022-05-17 14:34:59 +0900

    練習3

--- rensyu04/dodge_bomb.py
+++ rensyu04/dodge_bomb.py
@@ -11,8 +11,12 @@ def main():
     bg_img = pg.image.load("fig/pg_bg.jpg")   #背景画像用のsurface
     bg_rect = bg_img.get_rect()               #背景画像用のrect
     screen.blit(bg_img, bg_rect)              #背景画像用surfaceを画面用surfaceに貼り付けているイメージ
-    pg.display.update()
-    clock.tick(0.5)
+    #練習3
+    tori_img = pg.image.load("fig/3.png")     #こうかとん画像用のsurface
+    tori_img = pg.transform.rotozoom(tori_img, 0, 2)
+    tori_rect = tori_img.get_rect()           #こうかとん画像用のrect
+    tori_rect.center = 900, 400               #こうかとん画像用surfaceを画像用surfaceに貼り付ける
+    screen.blit(tori_img, tori_rect)
 
     while True:
         #練習2
